035fdbe08b56029e9542499e5061ba3c32ef3118
victoria <vika.feduanina@gmail.com>
2024-08-23 22:12:52 +0300

    configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import AutoImport from "unplugin-auto-import/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +16,21 @@ export default defineConfig({
     vueDevTools(),
     quasar({
       sassVariables: 'src/quasar-variables.sass'
-    })
+    }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+      ],
+      imports: [
+        'vue',
+        'vue-router',
+        'pinia',
+      ],
+      dts: './auto-imports.d.ts',
+      viteOptimizeDeps: true,
+    }),
   ],
   resolve: {
     alias: {
