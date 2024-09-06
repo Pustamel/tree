ad37746038c7c39de59b7f8925d554b53128d82b
victoria <vika.feduanina@gmail.com>
2024-09-06 09:31:21 +0300

    fix: router

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,9 +5,16 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      redirect: "/tree",
+      meta: { requiresAuth: false },
+      name: "-",
+    },
+    {
       path: '/tree',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: { requiresAuth: false },
     },
   ]
 })
